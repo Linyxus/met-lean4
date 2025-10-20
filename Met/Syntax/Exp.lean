5140f48217667d8f8b945cde10c3d28c8fe3a9f1
Yichen Xu <yichen.x@outlook.com>
2025-10-20 15:49:48 +0200

    Finish definition of expressions

--- Met/Syntax/Exp.lean
+++ Met/Syntax/Exp.lean
@@ -2,11 +2,19 @@ import Met.Syntax.Ty
 import Met.Debruijn
 namespace Met
 
+/-- Kinds of expressions. -/
 inductive ExpKind : Type where
 | value : ExpKind
 | term : ExpKind
 
+/-- Expressions. -/
 inductive Exp : ExpKind -> Sig -> Type where
 | unit : Exp .value s
+| var : BVar .var s -> Exp .value s
+| lam : Ty -> Exp k (s,x) -> Exp .value s
+| app : Exp k1 s -> Exp k2 s -> Exp .term s
+| mod : Modality -> Exp .value s -> Exp .value s
+| letmod : Modality -> Modality -> Exp .value s -> Exp k (s,x) -> Exp .term s
+| effdo : EffLabel -> Exp k s -> Exp .term s
 
 end Met
