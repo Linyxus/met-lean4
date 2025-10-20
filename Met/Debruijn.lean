36f2f2e8ff218d6a490d7d439796d2b340612cac
Yichen Xu <yichen.x@outlook.com>
2025-10-20 15:53:01 +0200

    Define renaming

--- Met/Debruijn.lean
+++ Met/Debruijn.lean
@@ -25,4 +25,24 @@ inductive BVar : BinderKind -> Sig -> Type where
   BVar k s ->
   BVar k (s,,k0)
 
+def Rename (s1 s2 : Sig) : Type := ∀ {k}, BVar k s1 -> BVar k s2
+
+def Rename.id : Rename s s := fun x => x
+
+def Rename.comp (f1 : Rename s1 s2) (f2 : Rename s2 s3) : Rename s1 s3 :=
+  fun x => f2 (f1 x)
+
+def BVar.rename {s1 s2 : Sig} (f : Rename s1 s2) {k : BinderKind}
+  (x : BVar k s1) : BVar k s2 :=
+  f x
+
+def Rename.lift (f : Rename s1 s2) : Rename (s1,,k) (s2,,k) :=
+  fun x =>
+    match x with
+    | BVar.here => BVar.here
+    | BVar.there y => BVar.there (f y)
+
+def Rename.succ : Rename s (s,,k) :=
+  fun x => BVar.there x
+
 end Met
--- Met/Syntax/Exp.lean
+++ Met/Syntax/Exp.lean
@@ -17,4 +17,13 @@ inductive Exp : ExpKind -> Sig -> Type where
 | letmod : Modality -> Modality -> Exp .value s -> Exp k (s,x) -> Exp .term s
 | effdo : EffLabel -> Exp k s -> Exp .term s
 
+def Exp.rename : Exp k s1 -> Rename s1 s2 -> Exp k s2
+| unit, _ => unit
+| var x, f => var (f x)
+| lam ty e, f => lam ty (e.rename f.lift)
+| app e1 e2, f => app (e1.rename f) (e2.rename f)
+| mod m e, f => mod m (e.rename f)
+| letmod m1 m2 e1 e2, f => letmod m1 m2 (e1.rename f) (e2.rename f.lift)
+| effdo l e, f => effdo l (e.rename f)
+
 end Met
